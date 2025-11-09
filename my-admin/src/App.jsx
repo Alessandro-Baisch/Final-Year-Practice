3fe1a6eb969f3fd30ed9ae776c081e27c24fa95b
Alessandro-Baisch <al.baisch@icloud.com>
2025-11-09 10:27:05 +0000

    commit

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,4 +1,4 @@
-import MyAdmin from "./admin";
+import MyAdmin from ".";
 
 const App = () => <MyAdmin />;
 
--- my-admin/src/index.jsx
+++ my-admin/src/index.jsx
@@ -1,12 +1,12 @@
 // in src/admin/index.tsx
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
-import {createTrailbaseProvider} from "/workspaces/Final-Year-Practice/my-admin/src/admin/index.jsx";
+import {createTrailbaseProvider} from "/workspaces/Final-Year-Practice/my-admin/ra-trailbase.js";
 
 const TRAILBASE_URL = "https://refactored-happiness-x59qj7xpwxv43694j-4000.app.github.dev/"
-const{dataProvider, authProvider} =await createTrailbaseProvider (TRAILBASE_URL);
+const{dataProvider} =await createTrailbaseProvider (TRAILBASE_URL);
 
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+  <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={ListGuesser} />
     <Resource name="comments" list={ListGuesser} />
   </Admin>

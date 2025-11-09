5e2d4a3b50b00eedab5df0c258d37a85847b7de2
Alessandro-Baisch <al.baisch@icloud.com>
2025-11-09 10:07:45 +0000

    commit

--- my-admin/src/admin/index.jsx
+++ my-admin/src/admin/index.jsx
@@ -1,6 +1,6 @@
 // in src/admin/index.tsx
 import { Admin, Resource, ListGuesser } from "react-admin";
-import {createTrailbaseProvider} from "ra-data-/workspaces/Final-Year-Practice/my-admin/ra-trailbase.js-server";
+import {createTrailbaseProvider} from "/workspaces/Final-Year-Practice/my-admin/src/admin/index.jsx";
 
 const TRAILBASE_URL = "https://refactored-happiness-x59qj7xpwxv43694j-4000.app.github.dev/"
 const{dataProvider, authProvider}=await createTrailbaseProvider(TRAILBASE_URL);
